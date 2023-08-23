878e4bc07b6e658e77136e045d8c44347c30187b
vetrovoy <v3trovoy@gmail.com>
2023-08-23 17:35:36 +0300

    Change messages holder

--- src/components/common/messagesHolder.tsx
+++ src/components/common/messagesHolder.tsx
@@ -5,40 +5,38 @@ import { message } from "antd";
 import { useTypedSelector } from "../../hooks/useTypedSelector.";
 
 export default function MessagesHolder() {
-  // const bots = useTypedSelector((state) => state.bots);
-  const user = useTypedSelector((state) => state.user);
+  const bots = useTypedSelector((state) => state.bots);
+  // const user = useTypedSelector((state) => state.user);
 
-  const [userMessageApi, userContextHolder] = message.useMessage();
-  // const [botsMessageApi, botsContextHolder] = message.useMessage();
-
-  useEffect(() => {
-    if (user.status !== "idle") {
-      userMessageApi.open({
-        type: user.status,
-        content: user.message,
-      });
-
-      return;
-    }
-  }, [user.status]);
+  // const [userMessageApi, userContextHolder] = message.useMessage();
+  const [botsMessageApi, botsContextHolder] = message.useMessage();
 
   // useEffect(() => {
-  //   if (bots.status !== "idle") {
-  //     botsMessageApi.open({
-  //       type: bots.status,
-  //       content: bots.message,
+  //   if (user.status !== "idle") {
+  //     userMessageApi.open({
+  //       type: user.status,
+  //       content: user.message,
   //     });
 
   //     return;
   //   }
-  // }, [bots.status]);
+  // }, [user.status]);
 
-  // console.log(user.status);
+  useEffect(() => {
+    if (bots.status !== "idle") {
+      botsMessageApi.open({
+        type: bots.status,
+        content: bots.message,
+      });
+
+      return;
+    }
+  }, [bots.status]);
 
   return (
     <>
-      {userContextHolder}
-      {/* {botsContextHolder} */}
+      {/* {userContextHolder} */}
+      {botsContextHolder}
     </>
   );
 }
